9d149cab9bdb672fba46b98e247eb41a7d822383
João Pedro Medici <JooPedroDalla>
2026-03-10 15:04:36 -0300

    Jogo resolvido

--- vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Jogo.java
+++ vapor/src/main/java/br/edu/insper/desagil/aps3/vapor/Jogo.java
@@ -1,10 +1,41 @@
 package br.edu.insper.desagil.aps3.vapor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Jogo {
     private int id;
     private String nome;
-    private Map<Usuario,Integer> avaliacoes;
+    private Map<Usuario,Integer> avaliacoes=new HashMap<>();
+
+    public Jogo(int id,String nome){
+        this.id=id;
+        this.nome=nome;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void avalia(Usuario apelido,int avaliacao){
+        avaliacoes.put(apelido,avaliacao);
+    }
+    public boolean avaliado(Usuario apelido){
+        return avaliacoes.containsKey(apelido);
+    }
+    public double media(){
+        int n=avaliacoes.size();
+        double valorMedio=0;
+        if (n>0){
+            for (int avaliacao:avaliacoes.values()) {
+                valorMedio += avaliacao;
+            }
+            return valorMedio;
+        }
+        else {
+            return 0;
+        }
+
+    }
 }
-/** parte do medici **/
